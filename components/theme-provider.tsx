7377593545b48e432a6d8d51c080fe735003e9d2
davi santana <davzn15@gmail.com>
2025-07-02 18:52:38 -0300

    fix: correção dos temas

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,26 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+interface CustomThemeProviderProps extends ThemeProviderProps {
+  children: React.ReactNode;
+  selectedTheme?: string; // Adiciona a prop para o tema selecionado
+}
+
+export function ThemeProvider({ children, selectedTheme, ...props }: CustomThemeProviderProps) {
+  // next-themes já adiciona a classe 'dark' ou 'light' ao html
+  // Precisamos adicionar a classe do tema customizado (ex: theme-brutalism)
+  React.useEffect(() => {
+    if (selectedTheme) {
+      document.documentElement.classList.add(`theme-${selectedTheme}`);
+    }
+    return () => {
+      if (selectedTheme) {
+        document.documentElement.classList.remove(`theme-${selectedTheme}`);
+      }
+    };
+  }, [selectedTheme]);
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
